9a6de94a2fd7237224be11588d10c0a8188723ba
Abraão F. Medeiros <abraaofmedeiros@gmail.com>
2024-10-05 11:13:50 -0300

    Primeiro commit da aplicação BuscaCursos

---
+++ src/Buscador.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Alura\BuscadorDeCursos;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+    /**
+     * Summary of httpClient
+     * @var ClientInterface
+     */
+    private $httpClient;
+
+    /**
+     * Summary of crawler
+     * @var Crawler
+     */
+    private $crawler;
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function buscar(string $url): array
+    {
+        $response = $this->httpClient->request("GET", $url);
+
+        $html = $response->getBody();
+        $this->crawler->addHtmlContent($html);
+        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
+        $cursos = [];
+
+        foreach ($elementosCursos as $elemento) {
+            $cursos[] = $elemento->textContent;
+        }
+
+        return $cursos;
+    }
+}
